1b5561aac8e7ee4621f06b14cc741edc999832f6
John Berry <jberry+githubcommits@lumoslabs.com>
2013-02-24 14:01:22 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Babytalk::Application.config.secret_token = '89e788caa5ad0f394670c4e629b201ea3f68712b75c7d724c57f9f9d8a7e2754c1d18c32ac53a370f87915c54c90a8827a9b0ac01ca6a378bf2379c4f9a7e776'
